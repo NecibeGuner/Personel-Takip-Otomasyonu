d41512c794f59acd0ba033fe2c49a65fd0212cea
Necibe Güner <221307049@kocaeli.edu.tr>
2024-07-22 14:30:37 +0300

    fixing bugs

--- Personel Takip Otomasyonu/frmPersonelMesaileri.Designer.cs
+++ Personel Takip Otomasyonu/frmPersonelMesaileri.Designer.cs
@@ -34,8 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "frmPersonelMesaileri";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Personel Mesaileri";
             this.ResumeLayout(false);
 
